f154b0eb03c9da286c54d345d7945850092e46c4
Brian Rodríguez <brianrp19@gmail.com>
2025-10-06 19:41:56 -0600

    Comentarios innecesarios

--- Workshop3/insertar.php
+++ Workshop3/insertar.php
@@ -8,12 +8,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $telefono  = $_POST['telefono'];
     $provincia = $_POST['provincia'];
 
-    // Insertar en la tabla usuarios
+ 
     $sql = "INSERT INTO usuarios (nombre, apellido, correo, telefono, provincia_id) 
             VALUES ('$nombre', '$apellido', '$correo', '$telefono', $provincia)";
 
     if ($conn->query($sql) === TRUE) {
-        // Redireccionar al login pasando el nombre de usuario
+     
         header("Location: login.php?username=" . urlencode($nombre));
         exit();
     } else {
